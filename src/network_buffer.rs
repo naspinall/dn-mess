235be7c5eb7842105d56bbf34515b47ef6c3e0b8
Nick Aspinall <nickhaspinall@gmail.com>
2022-01-25 22:59:06 -0500

    adding network buffer tests

--- src/network_buffer.rs
+++ src/network_buffer.rs
@@ -20,7 +20,7 @@ impl NetworkBuffer {
 
     pub fn put_u8(&mut self, byte: u8) -> BufferResult<()> {
         // Checking bounds
-        if self.write_cursor == MAX_MESSAGE_SIZE {
+        if self.write_cursor + 1 >= MAX_MESSAGE_SIZE {
             return Err(NetworkBufferError::BufferFullError);
         }
 
@@ -34,7 +34,7 @@ impl NetworkBuffer {
     }
 
     pub fn put_u16(&mut self, value: u16) -> BufferResult<()> {
-        if self.write_cursor + 2 == MAX_MESSAGE_SIZE {
+        if self.write_cursor + 2 >= MAX_MESSAGE_SIZE {
             return Err(NetworkBufferError::BufferFullError);
         }
 
@@ -47,7 +47,7 @@ impl NetworkBuffer {
     }
 
     pub fn put_u32(&mut self, value: u32) -> BufferResult<()> {
-        if self.write_cursor + 4 == MAX_MESSAGE_SIZE {
+        if self.write_cursor + 4 >= MAX_MESSAGE_SIZE {
             return Err(NetworkBufferError::BufferFullError);
         }
 
@@ -111,5 +111,124 @@ impl NetworkBuffer {
 
     pub fn len(&self) -> usize {
         self.write_cursor
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_put_u8() {
+        let mut buf = NetworkBuffer::new();
+        buf.put_u8(0xFF).unwrap();
+
+        assert_eq!(buf.buf[0], 0xFF);
+        assert_eq!(buf.len(), 1);
+    }
+
+    #[test]
+    fn test_put_u16() {
+        let mut buf = NetworkBuffer::new();
+        buf.put_u16(0xFC3F).unwrap();
+
+        assert_eq!(buf.buf[0], 0xFC);
+        assert_eq!(buf.buf[1], 0x3F);
+        assert_eq!(buf.len(), 2);
+    }
+
+    #[test]
+    fn test_put_u32() {
+        let mut buf = NetworkBuffer::new();
+        buf.put_u32(0x12345678).unwrap();
+
+        assert_eq!(buf.buf[0], 0x12);
+        assert_eq!(buf.buf[1], 0x34);
+        assert_eq!(buf.buf[2], 0x56);
+        assert_eq!(buf.buf[3], 0x78);
+        assert_eq!(buf.len(), 4);
+    }
+
+    #[test]
+    fn test_put_u8_at_capacity() {
+        let mut buf = NetworkBuffer::new();
+
+        buf.write_cursor = MAX_MESSAGE_SIZE;
+
+        assert!(buf.put_u8(0xFF).is_err());
+
+        buf.write_cursor = MAX_MESSAGE_SIZE - 1;
+
+        assert!(buf.put_u8(0xFF).is_err());
+    }
+
+    #[test]
+    fn test_put_u16_at_capacity() {
+        let mut buf = NetworkBuffer::new();
+
+        buf.write_cursor = MAX_MESSAGE_SIZE;
+        assert!(buf.put_u16(0xFFFF).is_err());
+
+        buf.write_cursor = MAX_MESSAGE_SIZE - 1;
+        assert!(buf.put_u16(0xFFFF).is_err());
+
+        buf.write_cursor = MAX_MESSAGE_SIZE - 2;
+        assert!(buf.put_u16(0xFFFF).is_err());
+    }
+
+    #[test]
+    fn test_put_u32_at_capacity() {
+        let mut buf = NetworkBuffer::new();
+
+        buf.write_cursor = MAX_MESSAGE_SIZE;
+        assert!(buf.put_u32(0xFFFF).is_err());
+        buf.write_cursor = MAX_MESSAGE_SIZE - 1;
+
+        assert!(buf.put_u32(0xFFFF).is_err());
+        buf.write_cursor = MAX_MESSAGE_SIZE - 2;
+
+        assert!(buf.put_u32(0xFFFF).is_err());
+        buf.write_cursor = MAX_MESSAGE_SIZE - 3;
+
+        assert!(buf.put_u32(0xFFFF).is_err());
+        buf.write_cursor = MAX_MESSAGE_SIZE - 4;
+        assert!(buf.put_u32(0xFFFF).is_err());
+    }
+
+    #[test]
+    fn test_get_u8() {
+        let mut buf = NetworkBuffer::new();
+        buf.buf[0] = 0xFF;
+
+        let value = buf.get_u8().unwrap();
+
+        assert_eq!(value, 0xFF);
+        assert_eq!(buf.read_cursor, 1);
+    }
+
+    #[test]
+    fn test_get_u16() {
+        let mut buf = NetworkBuffer::new();
+        buf.buf[0] = 0xFF;
+        buf.buf[1] = 0x11;
+
+        let value = buf.get_u16().unwrap();
+
+        assert_eq!(value, 0xFF11);
+        assert_eq!(buf.read_cursor, 2);
+    }
+
+    #[test]
+    fn test_get_u32() {
+        let mut buf = NetworkBuffer::new();
+        buf.buf[0] = 0xFF;
+        buf.buf[1] = 0x11;
+        buf.buf[2] = 0x22;
+        buf.buf[3] = 0x33;
+
+        let value = buf.get_u32().unwrap();
+
+        assert_eq!(value, 0xFF112233);
+        assert_eq!(buf.read_cursor, 4);
     }
 }
