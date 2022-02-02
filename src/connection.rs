2d13c2150274a45b2b2811c72bf41b7bc2b066d7
Nick Aspinall <nickhaspinall@gmail.com>
2022-02-02 18:10:14 -0500

    trying move, not sure if required

--- src/connection.rs
+++ src/connection.rs
@@ -1,4 +1,4 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::net::SocketAddr;
 
 use tokio::net::UdpSocket;
 
@@ -7,42 +7,28 @@ use crate::{coding::FrameCoder, network_buffer::NetworkBuffer, packets::Frame};
 type ConnectionResult<T> = Result<T, Box<dyn std::error::Error>>;
 
 pub struct Connection {
-    sock: UdpSocket,
     buf: NetworkBuffer,
 }
 
 impl Connection {
-    pub async fn listen(port: usize) -> ConnectionResult<Connection> {
+    pub fn new() -> Connection {
         // Initializing buffers
         let buf = NetworkBuffer::new();
 
-        // Bind to socket to listen for responses
-        let sock = UdpSocket::bind(format!("0.0.0.0:{}", port)).await?;
-
-        Ok(Connection { sock, buf })
+        Connection { buf }
     }
 
-    pub async fn connect(addr: &SocketAddr) -> ConnectionResult<Connection> {
-        let buf = NetworkBuffer::new();
-
-        // Create new socket on OS provided port
-        let sock_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0);
-
-        let sock = UdpSocket::bind(sock_addr).await?;
-
-        // Connecting to the given addr, only accepting packets from this addr
-        sock.connect(addr).await?;
-
-        Ok(Connection { buf, sock })
-    }
-
-    pub async fn write_frame(&mut self, frame: &Frame, addr: &SocketAddr) -> ConnectionResult<()> {
+    pub async fn write_frame(
+        &mut self,
+        sock: &UdpSocket,
+        frame: &Frame,
+        to_addr: &SocketAddr,
+    ) -> ConnectionResult<()> {
         FrameCoder::new().encode_frame(frame, &mut self.buf)?;
 
         let buffer_length = self.buf.len();
 
-        self.sock
-            .send_to(&self.buf.buf[..buffer_length], addr)
+        sock.send_to(&self.buf.buf[..buffer_length], to_addr)
             .await?;
 
         // Reset buffer for reuse
@@ -51,8 +37,8 @@ impl Connection {
         Ok(())
     }
 
-    pub async fn read_frame(&mut self) -> ConnectionResult<(SocketAddr, Frame)> {
-        let (_len, addr) = self.sock.recv_from(&mut self.buf.buf).await?;
+    pub async fn read_frame(&mut self, sock: &UdpSocket) -> ConnectionResult<(SocketAddr, Frame)> {
+        let (_len, addr) = sock.recv_from(&mut self.buf.buf).await?;
 
         let frame = FrameCoder::new().decode_frame(&mut self.buf)?;
 
