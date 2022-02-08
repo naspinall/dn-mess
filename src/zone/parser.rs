67cc5c83104bcdb18a0001310ca661bfe514fccd
Nick Aspinall <nickhaspinall@gmail.com>
2022-02-08 18:20:00 -0500

    added simple parsing for a zone file

--- src/zone/parser.rs
+++ src/zone/parser.rs
@@ -3,13 +3,36 @@
 // Parse as normal
 
 use core::fmt;
+use std::{
+    net::{Ipv4Addr, Ipv6Addr},
+    str::SplitWhitespace,
+};
 use tokio::fs;
+
+struct ZoneFile {
+    name: String,
+    time_to_live: usize,
+    records: Vec<RecordData>,
+}
 
 #[derive(Debug)]
 pub enum ZoneParserError {
     EmptyFile,
     NoOrigin,
     NoTimeToLive,
+    NoDomain,
+    NoClass,
+    InvalidClass,
+    NoType,
+    InvalidType,
+    DataParsing,
+}
+
+enum RecordData {
+    ARecord(Ipv4Addr),
+    AAAARecord(Ipv6Addr),
+    MXRecord(usize, String),
+    CName(String),
 }
 
 impl std::error::Error for ZoneParserError {}
@@ -20,18 +43,22 @@ impl fmt::Display for ZoneParserError {
             ZoneParserError::EmptyFile => write!(f, "Empty file"),
             ZoneParserError::NoOrigin => write!(f, "No origin provided"),
             ZoneParserError::NoTimeToLive => write!(f, "No time to live provided"),
+            ZoneParserError::NoDomain => write!(f, "No domain provided"),
+            ZoneParserError::NoClass => write!(f, "No class provided"),
+            ZoneParserError::InvalidClass => write!(f, "Invalid class provided"),
+            ZoneParserError::NoType => write!(f, "type provided"),
+            ZoneParserError::InvalidType => write!(f, "Invalid class provided"),
+            ZoneParserError::DataParsing => write!(f, "Error parsing data"),
         }
     }
 }
 
 type ParserResult<T> = Result<T, Box<dyn std::error::Error>>;
 
-async fn parse(path: &str) -> ParserResult<()> {
-    // Loading file into bytes
-    let file_bytes = fs::read(path).await?;
-
+fn parse(bytes: Vec<u8>) -> ParserResult<ZoneFile> {
     // Bytes to string
-    let file = String::from_utf8(file_bytes)?;
+    let file = String::from_utf8(bytes)?;
+    let file = file.trim();
 
     // Split on newline
     let mut lines = file.lines().map(|line| {
@@ -49,7 +76,12 @@ async fn parse(path: &str) -> ParserResult<()> {
     };
 
     // Parsing the zone name
-    let zone_name = line.next().ok_or_else(|| ZoneParserError::NoOrigin)?;
+    let name = line
+        .next()
+        .ok_or_else(|| ZoneParserError::NoOrigin)?
+        .to_string();
+
+    let mut line = lines.next().ok_or_else(|| ZoneParserError::EmptyFile)?;
 
     // Parse time to live
     match line.next() {
@@ -58,10 +90,149 @@ async fn parse(path: &str) -> ParserResult<()> {
     };
 
     // Parsing the zone name
-    let time_to_live: i64 = line
+    let time_to_live: usize = line
         .next()
         .ok_or_else(|| ZoneParserError::NoTimeToLive)?
         .parse()?;
 
-    Ok(())
+    // Use to keep track of the previous domain, used when finding '@' symbols that refer to the last domain
+    let mut previous_domain: Option<&str> = None;
+
+    let mut records = Vec::new();
+
+    // Parse the rest of the file
+    for mut line in lines {
+        let mut domain = line.next().ok_or_else(|| ZoneParserError::NoDomain)?;
+
+        if domain == "@" {
+            // Set to the last domain if it exists
+            domain = previous_domain.ok_or_else(|| ZoneParserError::NoDomain)?;
+        }
+
+        // Parse the class and the record type
+        // Only want to parse "IN" values
+        match line.next().ok_or_else(|| ZoneParserError::NoClass)? {
+            "IN" => {}
+            _ => return Err(Box::new(ZoneParserError::InvalidClass)),
+        };
+
+        let record_type = line.next().ok_or_else(|| ZoneParserError::NoType)?;
+
+        // Parse the data and reject a bad type
+        let data = match record_type {
+            "A" => RecordData::ARecord(parse_a_record_data(&mut line)?),
+            "AAAA" => RecordData::AAAARecord(parse_aaaa_record_data(&mut line)?),
+            "CNAME" => RecordData::CName(parse_cname_record_data(&mut line)?),
+            "MX" => {
+                let (priority, domain) = parse_mx_record_data(&mut line)?;
+                RecordData::MXRecord(priority, domain)
+            }
+            _ => return Err(Box::new(ZoneParserError::InvalidType)),
+        };
+
+        // Add record
+        records.push(data);
+
+        // Set previous domain
+        previous_domain = Some(domain);
+    }
+
+    Ok(ZoneFile {
+        name,
+        time_to_live,
+        records,
+    })
+}
+
+fn parse_a_record_data(line: &mut SplitWhitespace) -> ParserResult<Ipv4Addr> {
+    // Get only one column
+    let address_string = line.next().ok_or_else(|| ZoneParserError::DataParsing)?;
+
+    // Parse into an ip address
+    let address: Ipv4Addr = address_string.parse()?;
+
+    Ok(address)
+}
+
+fn parse_aaaa_record_data(line: &mut SplitWhitespace) -> ParserResult<Ipv6Addr> {
+    // Get only one column
+    let address_string = line.next().ok_or_else(|| ZoneParserError::DataParsing)?;
+
+    // Parse into an ip address
+    let address: Ipv6Addr = address_string.parse()?;
+
+    Ok(address)
+}
+
+fn parse_mx_record_data(line: &mut SplitWhitespace) -> ParserResult<(usize, String)> {
+    // Get only one column
+    let priority = line
+        .next()
+        .ok_or_else(|| ZoneParserError::DataParsing)?
+        .parse()?;
+
+    // Get only one column
+    let domain = line
+        .next()
+        .ok_or_else(|| ZoneParserError::DataParsing)?
+        .to_string();
+
+    Ok((priority, domain))
+}
+
+fn parse_cname_record_data(line: &mut SplitWhitespace) -> ParserResult<String> {
+    // Get only one column
+    let cname = line.next().ok_or_else(|| ZoneParserError::DataParsing)?;
+
+    Ok(cname.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_simple_zone_file() {
+        let file = "
+            $ORIGIN example.com.     ; designates the start of this zone file in the namespace
+            $TTL 3600                ; default expiration time (in seconds) of all RRs without their own TTL value
+            example.com.  IN  MX    10 mail.example.com.  ; mail.example.com is the mailserver for example.com
+            example.com.  IN  CNAME example.com ; More comments
+            @             IN  A     127.0.0.1
+            @             IN  AAAA  2001:db8:10::2        ; IPv6 address for ns.example.com
+        ";
+
+        let results = parse(file.as_bytes().to_vec()).unwrap();
+
+        assert_eq!(results.name, "example.com.");
+        assert_eq!(results.time_to_live, 3600);
+
+        let mx_record = results.records.get(0).unwrap();
+        match mx_record {
+            RecordData::MXRecord(priority, domain) => {
+                assert_eq!(domain, "mail.example.com.");
+                assert_eq!(*priority, 10)
+            }
+            _ => panic!("Bad match"),
+        }
+        let cname_record = results.records.get(1).unwrap();
+        match cname_record {
+            RecordData::CName(domain) => assert_eq!(domain, "example.com"),
+            _ => panic!("Bad match"),
+        }
+        let a_record = results.records.get(2).unwrap();
+        match a_record {
+            RecordData::ARecord(record) => {
+                assert_eq!("127.0.0.1".parse::<Ipv4Addr>().unwrap(), *record)
+            }
+            _ => panic!("Bad match"),
+        }
+        let aaaa_record = results.records.get(3).unwrap();
+        match aaaa_record {
+            RecordData::AAAARecord(record) => {
+                assert_eq!("2001:db8:10::2".parse::<Ipv6Addr>().unwrap(), *record)
+            }
+            _ => panic!("Bad match"),
+        }
+    }
 }
